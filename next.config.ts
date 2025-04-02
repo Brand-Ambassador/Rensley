547a4e9d23f4f9a87bbfb3c337b1ed46e1146eb3
Rensley <ranly196@gmail.com>
2025-04-02 14:06:06 -0400

    Updates

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === "production" ? "/Rensley" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/Rensley/" : "",
-};
-
-module.exports = nextConfig;
